2fa19965f89963e0f54eeb1a4520c0c1b30a5f06
Natashaum <natashaum3177@gmail.com>
2022-07-23 00:58:35 +0300

    HW_1

---
+++ Lesson 1/main.js
@@ -0,0 +1,95 @@
+// // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16,
+// // true, false.
+// //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let a = 'hello';
+// // document.write(a+' ');
+// console.log(a);
+// alert(a);
+//
+// let b = 'owu';
+// // document.write(b+' ');
+// console.log(b);
+// alert(b);
+//
+// let c = 'com';
+// // document.write(c+' ');
+// console.log(c);
+// alert(c);
+//
+// let d = 'ua';
+// // document.write(d+' ');
+// console.log(d);
+// alert(d);
+//
+// let e = 1;
+// // document.write(e+' ');
+// console.log(e);
+// alert(e);
+//
+// let f = 10;
+// // document.write(f+' ');
+// console.log(f);
+// alert(f);
+//
+// let g = -999;
+// // document.write(g+' ');
+// console.log(g);
+// alert(g);
+//
+// let h = 123;
+// // document.write(h+' ');
+// console.log(h);
+// alert(h);
+//
+// let i = 3.14;
+// // document.write(i+' ');
+// console.log(i);
+// alert(i);
+//
+// let j = 2.7;
+// // document.write(j+' ');
+// console.log(j);
+// alert(j);
+//
+// let k = 16;
+// // document.write(k+' ');
+// console.log(k);
+// alert(k);
+//
+// let l = true;
+// // document.write(l+' ');
+// console.log(l);
+// alert(l);
+//
+// let m = false;
+// // document.write(m+' ');
+// console.log(m);
+// alert(m);
+//
+// document.write(a+' '+b+' '+c+' '+d+' '+e+' '+f+' '+g+' '+h+' '+i+' '+j+' '+k+' '+l+' '+m);
+
+// // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// let firstName = 'Nataliia';
+// let middleName = 'Andriivna';
+// let lastName = 'Fedorovych';
+//
+// document.write(firstName+' '+middleName+' '+lastName )
+
+//
+// // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// //     let a = 100; let b = '100'; let c = true;
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+//
+// // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+let firstName = prompt('Enter your firstname');
+let middleName = prompt('Enter your middleName');
+let surName = prompt('Enter your surName');
+
+console.log(firstName+' '+middleName+' '+surName);
+document.write(firstName+' '+middleName+' '+surName);
+
