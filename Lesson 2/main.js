93226e39c80e77ae91dd7c708583aac09cae654c
Natashaum <natashaum3177@gmail.com>
2022-07-30 00:34:05 +0300

    HW 2

---
+++ Lesson 2/main.js
@@ -0,0 +1,149 @@
+// // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let arr = ['Hello!', 'Everyone,', 'who', 'is', 'older', 'than', [25, 30, 35,40,45], 'years', 'old', {status: 'married'}];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+// console.log(arr[8]);
+// console.log(arr[9]);
+
+
+// // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let book1 = {
+//     title: 'The Da vinci code',
+//     pageCount: 268,
+//     genre: 'novel'
+// }
+// let book2 = {
+//     title: 'Angels & Demons',
+//     pageCount: 252,
+//     genre: 'novel'
+// }
+// let book3 = {
+//     title: 'The Lost Symbol',
+//     pageCount: 300,
+//     genre: 'novel'
+// }
+//     console.log(book1, book2, book3);
+
+
+// // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+//
+// let book1 = {
+//     title: 'The Da vinci code',
+//     pageCount: 268,
+//     genre: 'novel',
+//     authors: [
+//         {
+//             name:'Dan Brown',
+//             age: 58
+//         },
+//         {
+//             name: 'Vasia Bubkin',
+//             age: 36
+//         }
+//     ]
+// }
+// let book2 = {
+//     title: 'Angels & Demons',
+//     pageCount: 252,
+//     genre: 'novel',
+//     authors: [
+//         {
+//             name:'Dan Brown',
+//             age: 58
+//         },
+//         {
+//             name: 'John Lii',
+//             age: 35
+//         }
+//     ]
+//
+// }
+// let book3 = {
+//     title: 'The Lost Symbol',
+//     pageCount: 300,
+//     genre: 'novel',
+//     authors: [
+//         {
+//             name:'Dan Brown',
+//             age: 58
+//         },
+//         {
+//             name: 'Borys Jonson',
+//             age: 58
+//         }
+//     ]
+// }
+// console.log(book1, book2, book3);
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+let users = [
+    {
+        name: 'Vasia',
+        username: 'Vasiatko',
+        password: 'Vas - vas'
+    },
+    {
+        name: 'Sasha',
+        username: 'Sanok',
+        password: 'SanSanych'
+    },
+    {
+        name: 'Anna',
+        username: 'Anettt33',
+        password: 'Ann25'
+    },
+    {
+        name: 'Kyzia',
+        username: 'Kyzmuch',
+        password: 'Ky35zia'
+
+    },
+    {
+        name: 'Nafania',
+        username: 'Nafnaf',
+        password: 'Naf123'
+    },
+    {
+        name: 'Vasuluna',
+        username: 'Thikava',
+        password: 'Vas555'
+    },
+    {
+        name: 'Susan',
+        username: 'Sue',
+        password: 'Sushechka11'
+    },
+    {
+        name: 'John',
+        username: 'JohnLii',
+        password: 'JoLii35'
+    },
+    {
+        name: 'Tom',
+        username: 'Tomuch',
+        password: 'Tomakha333'
+
+    },
+    {
+        name: 'Jerry',
+        username: 'Jerka',
+        password: 'Jer_122'
+    }
+];
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
